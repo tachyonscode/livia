3b6671e67e8d603d280007a5757540db5506687e
Leonardo Mangolin <l.mangolin.ext@conductor.com.br>
2019-07-08 00:09:32 -0300

    Primeiro commit

---
+++ src/main/java/br/com/dynamicit/livia/LiviaApplication.java
@@ -0,0 +1,13 @@
+package br.com.dynamicit.livia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiviaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiviaApplication.class, args);
+	}
+
+}
